7e4070d5723c5ad8f6debea7e149f6aeedc93e0f
Aleksandr Zvetkov <seel.ru@gmail.com>
2018-08-02 00:17:37 +0300

    Add project files.

---
+++ SimpleOrgMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleOrgMVC.MvcApplication" Language="C#" %>
---
+++ SimpleOrgMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleOrgMVC.Startup))]
+namespace SimpleOrgMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
